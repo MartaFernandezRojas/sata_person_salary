b0e14f196521eb335e7bdc0fc0c3a86259bba597
Marta Fernandez <f.m.rojas@hotmail.com>
2021-03-16 20:47:09 +0100

    create methods printSalary & delay

--- src/components/salary/salary.vue
+++ src/components/salary/salary.vue
@@ -3,20 +3,20 @@
 <div id="search">
     <div class="head">
         <el-row>
-            <el-col :span="12" :offset="2">
+            <el-col :span="10" :offset="2">
                 <h1 class="title">Salary average</h1>
             </el-col>
-               <el-col :span="4" :offset="2">
-               <el-button type="warning" @click="showSalary()">Show salary</el-button>
+               <el-col :span="4" :offset="2" class="margin-top">
+               <el-button type="warning" @click="printSalary()">Show salary</el-button>
+                <span class="title" v-if="average!=0">AVG: {{Math.round(average)}}€</span>
             </el-col>
         </el-row>
-
     </div>
     <el-row>
         <el-col :span="22" :offset="1">
             <h3 class="subtitle">Results</h3>
             <!--Result table-->
-            <el-table class="table" :data="resultsFilter" v-loading="loading">
+            <el-table class="margin-top" :data="resultsFilter" >
                 <el-table-column prop="lastname" label="Name" class="title">
                    <template slot-scope="scope">
                        {{scope.row.lastname}}, {{scope.row.firstname}}
@@ -25,6 +25,9 @@
                 <el-table-column prop="age" label="Age" class="title">
                 </el-table-column>
                 <el-table-column prop="salary" label="Salary" class="title" align="center">
+                 <template slot-scope="scope" >
+                       {{scope.row.salary}} <span  v-if="scope.row.salary!='calculating...'">€</span>
+                    </template>
                 </el-table-column>
             </el-table>
         </el-col>
